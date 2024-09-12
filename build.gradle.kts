fbeb9bc7d531d0cf896e9ce2ddb07b86e52e7b78
Didier Garcia <digarcia@twilio.com>
2024-09-12 10:58:37 -0400

    feat: update jetbrains kotlin plugin to 1.8.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = getVersionName()
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.6.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
